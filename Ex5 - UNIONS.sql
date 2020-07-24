2b25967e4ad13331d7bc7c169543fe0c6f52644c
Cecilia Laborde <cecilia@patchwork.health>
2020-07-24 16:19:39 +0200

    Unions

---
+++ Ex5 - UNIONS.sql
@@ -0,0 +1,28 @@
+SELECT
+	customer_id,
+	first_name,
+    points,
+    "Bronze" as type
+FROM customers
+WHERE points < 2000
+
+UNION
+
+SELECT
+	customer_id,
+	first_name,
+    points,
+    "Silver" as type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+
+UNION
+
+SELECT
+	customer_id,
+	first_name,
+    points,
+    "Gold" as type
+FROM customers
+WHERE 3000 <= points 
+ORDER BY first_name
